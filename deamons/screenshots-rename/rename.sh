aa3281e0acf8b242e309225289300f0ec2dff619
Matej Košiarčik <matej.kosiarcik@gmail.com>
2023-06-11 11:40:56 +0200

    feat: allow non-english Screenshot and Screensrecording filenames

--- deamons/screenshots-rename/rename.sh
+++ deamons/screenshots-rename/rename.sh
@@ -18,11 +18,11 @@ cd "$(dirname "$file")"
 old_filename="$(basename "$file")"
 
 # Exit on files not in proper format
-if printf '%s\n' "$old_filename" | grep -vE '^Screenshot [0-9\-]+ at [0-9\.]+( \([0-9]+\))?\.png$' >/dev/null; then
+if printf '%s\n' "$old_filename" | grep -vE '^.+ [0-9\-]+ at [0-9\.]+( \([0-9]+\))?\.png$' >/dev/null; then
     exit 0
 fi
 
-date="$(printf '%s\n' "$(basename "$file")" | sed -E 's~^Screenshot ([0-9]{4})-([0-9]{2})-([0-9]{2}) at ([0-9]{2})\.([0-9]{2})\.([0-9]{2})( \([0-9]+\))?\.png$~\1-\2-\3_\4-\5-\6~')"
+date="$(printf '%s\n' "$(basename "$file")" | sed -E 's~^.+ ([0-9]{4})-([0-9]{2})-([0-9]{2}) at ([0-9]{2})\.([0-9]{2})\.([0-9]{2})( \([0-9]+\))?\.png$~\1-\2-\3_\4-\5-\6~')"
 new_filename="$date.png"
 
 # Check if the new file already exists (taken screenshots of multiple displays)
@@ -30,8 +30,8 @@ if [ -e "$date 2.png" ] || [ -e "$date 3.png" ] || [ -e "$date 4.png" ]; then
     new_filename="$date 1.png"
 fi
 
-if printf '%s\n' "$old_filename" | grep -E '^Screenshot [0-9\-]+ at [0-9\.]+ \([0-9]+\)\.png$' >/dev/null; then
-    index="$(printf '%s\n' "$old_filename" | sed -E 's~^Screenshot [0-9\-]+ at [0-9\.]+ \(([0-9]+)\)\.png$~\1~')"
+if printf '%s\n' "$old_filename" | grep -E '^.+ [0-9\-]+ at [0-9\.]+ \([0-9]+\)\.png$' >/dev/null; then
+    index="$(printf '%s\n' "$old_filename" | sed -E 's~^.+ [0-9\-]+ at [0-9\.]+ \(([0-9]+)\)\.png$~\1~')"
     new_filename="$date $index.png"
 
     # Check if the new file already exists (taken screenshots of multiple displays)
